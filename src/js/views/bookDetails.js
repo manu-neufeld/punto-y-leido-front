4574400a70990f9d4e7eebfb99872397f1ca7ac1
crduque <crduque@hotmail.com>
2020-11-18 12:08:46 +0000

    created component card book details and view book details

---
+++ src/js/views/bookDetails.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+// import "../../styles/home.scss";
+import {CardBookDetails} from "../component/cardBookDetails.js"
+
+export const BookDetails = () => {
+    <Fragment>
+        <div>
+            <CardBookDetails />
+        </div>
+    </Fragment>
+};
