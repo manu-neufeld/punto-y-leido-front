7348b9fe975305c2ee35589f82617b6fc5c86940
manu-neufeld <manuela.neufeld@gmail.com>
2020-11-18 12:02:51 +0000

    view created, and card created

---
+++ src/js/component/cardAuthorDetail.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export const AuthorDetail = () => {
+
+return(
+    <div class="card mb-3" style="max-width: 540px;">
+        <div class="row no-gutters">
+            <div class="col-md-4">
+                <img src="#" class="card-img" alt="...">
+                </img>
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">Author</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+} 
