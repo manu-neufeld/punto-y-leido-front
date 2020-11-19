2eca08b56c5f59894d8980dd23a9d7b945e245c2
manu-neufeld <manuela.neufeld@gmail.com>
2020-11-19 10:16:23 +0000

    personal card create and import in the view

--- src/js/views/authorDetails.js
+++ src/js/views/authorDetails.js
@@ -1,11 +1,17 @@
 import React, { Fragment } from "react";
 import "../../styles/home.scss";
 import { AuthorDetail } from "../component/cardAuthorDetail.jsx";
+import { LittleBookCover } from "../component/littleBookCover.jsx";
 
 export const AuthorDetails = () => {
 	return (
 		<Fragment>
-			<div>{<AuthorDetail />}</div>
+			<div>
+				<AuthorDetail />
+			</div>
+			<div>
+				<LittleBookCover />
+			</div>
 		</Fragment>
 	);
 };
