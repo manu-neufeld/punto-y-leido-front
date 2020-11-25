3086e197519ce1186e9c216ca9e7b27b6b2f0725
crduque <crduque@hotmail.com>
2020-11-25 16:54:03 +0000

    changed some style in component welcome

--- src/js/component/welcomeHome.jsx
+++ src/js/component/welcomeHome.jsx
@@ -4,9 +4,9 @@ import "../../styles/welcome-component.scss";
 export const WelcomeHome = () => {
 	return (
 		<div className="welcome-text">
-			<h2>¡Bienvenido/a </h2>
-			<h3>a</h3>
-			<h2>Punto y leído!</h2>
+			<p>Bienvenido/a</p>
+			<p>a</p>
+			<p>Punto y leído!</p>
 		</div>
 	);
 };
