4733679f432afafba89873c187877912a040309d
manu-neufeld <manuela.neufeld@gmail.com>
2020-11-30 17:33:15 +0000

    fetch fail, i do not know why

--- src/js/component/cardBookDetails.js
+++ src/js/component/cardBookDetails.js
@@ -1,14 +1,49 @@
-import React, { Fragment } from "react";
-import { Link } from "react-router-dom";
+import React, { Fragment, useContext, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
 import PropTypes from "prop-types";
 import "../../styles/card-book-details-component.scss";
+import { Context } from "../store/appContext";
 
 export const CardBookDetails = props => {
+	const { store, actions } = useContext(Context);
+	let params = useParams();
+
+	useEffect(() => {
+		actions.getAllShelfInfoTest(params.id_book, params.id_reader);
+	}, []);
+
 	return (
 		<Fragment>
 			<div className="card">
 				<div className="row no-gutters">
 					<div className="col-md-4 book-image">
+						<div className="btn-group">
+							<button
+								type="button"
+								className="btn btn-secondary dropdown-toggle"
+								data-toggle="dropdown"
+								data-display="static"
+								aria-haspopup="true"
+								aria-expanded="false"
+							/>
+							<div className="dropdown-menu dropdown-menu-lg-right">
+								<button
+									className="dropdown-item"
+									type="button"
+									onClick={() => {
+										actions.postBookOnShelf(params.idBook, 3, "favoritos");
+										console.log("store: ", params);
+									}}>
+									Favorito
+								</button>
+								<button className="dropdown-item" type="button">
+									Leído
+								</button>
+								<button className="dropdown-item" type="button">
+									Pendiente
+								</button>
+							</div>
+						</div>
 						<img src={props.image} className="card-img" alt="Portada del libro" />
 					</div>
 					<div className="col-md-8 book-info">
--- src/js/store/appContext.js
+++ src/js/store/appContext.js
@@ -26,6 +26,7 @@ const injectContext = PassedComponent => {
 			state.actions.getWrittenBy();
 			state.actions.getReviews();
 			state.actions.getAllAuthorInfo();
+			state.actions.getAllShelfInfoTest();
 		}, []);
 
 		// The initial value for the context is not null anymore, but the current state of this component,
