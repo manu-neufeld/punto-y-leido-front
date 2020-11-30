d251a79c8eb2c66558854ef3891bbaca90801a8b
crduque <crduque@hotmail.com>
2020-11-30 11:36:40 +0000

    added our logo to the navbar and to home view

--- src/js/component/aboutUsHome.jsx
+++ src/js/component/aboutUsHome.jsx
@@ -1,10 +1,11 @@
 import React, { Fragment } from "react";
+import logo from "../../img/logo-punto-y-leido.png";
 import "../../styles/about-us-component.scss";
 
 export const AboutUsHome = () => {
 	return (
 		<div className="about-us-home">
-			<h1>Punto y leído</h1>
+			<img src={logo} alt="Punto y leído logo" className="logo-header" />
 			<p className="about-us-text">
 				Donut carrot cake cheesecake cake cake ice cream jujubes. Cake cake halvah. Tiramisu soufflé apple pie
 				ice cream. Fruitcake liquorice macaroon donut cookie tootsie roll. Jujubes croissant sugar plum I love
--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,11 +1,13 @@
 import React from "react";
+import logo from "../../img/logo-punto-y-leido.png";
 import { Link } from "react-router-dom";
+import "../../styles/navbar.scss";
 
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<img src={logo} alt="Punto y leído logo" className="navbar-brand logo-navbar" />
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
