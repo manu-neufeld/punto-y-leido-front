4de86f47f2df59344a4793094acfb46c290978a9
manu-neufeld <manuela.neufeld@gmail.com>
2020-12-01 16:32:03 +0000

    styles

--- src/js/component/littleBookCover.jsx
+++ src/js/component/littleBookCover.jsx
@@ -1,9 +1,57 @@
-import React from "react";
+import React, { Fragment, useContext, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
 import PropTypes from "prop-types";
+import { Context } from "../store/appContext";
 
 export const LittleBookCover = () => {
+	const { store, actions } = useContext(Context);
+	let params = useParams();
+
+	useEffect(() => {
+		actions.postBookOnShelf(params.id_book, params.id_reader);
+	}, []);
+
 	return (
 		<div className="card">
+			<div className="btn-group boton">
+				<button
+					type="button"
+					className="btn btn-secondary dropdown-toggle"
+					data-toggle="dropdown"
+					data-display="static"
+					aria-haspopup="true"
+					aria-expanded="false"
+				/>
+				<div className="dropdown-menu dropdown-menu-lg-right">
+					<button
+						className="dropdown-item"
+						type="button"
+						onClick={() => {
+							actions.postBookOnShelf(params.idBook, 4, "favoritos");
+							console.log("pamametros pasados ", params.id_book, params.id_reader, "favoritos");
+						}}>
+						Favorito
+					</button>
+					<button
+						className="dropdown-item"
+						type="button"
+						onClick={() => {
+							actions.postBookOnShelf(params.idBook, 6, "leidos");
+							console.log("pamametros pasados ", params);
+						}}>
+						Leído
+					</button>
+					<button
+						className="dropdown-item"
+						type="button"
+						onClick={() => {
+							actions.postBookOnShelf(params.idBook, 5, "pendientes");
+							console.log("pamametros pasados ", params);
+						}}>
+						Pendiente
+					</button>
+				</div>
+			</div>
 			<img src="#" className="card-img-top" alt="..." />
 			<div className="card-body">
 				<h5 className="card-title">Título</h5>
