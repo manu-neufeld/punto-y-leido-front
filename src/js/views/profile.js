7304f4e71cc5657753402efcd7099105a082a1a6
crduque <crduque@hotmail.com>
2020-12-09 11:09:04 +0000

    now you can only edit the reader info if you're seeing the profile of the reader that's logged

--- src/js/views/profile.js
+++ src/js/views/profile.js
@@ -1,26 +1,31 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useContext } from "react";
+import { useParams } from "react-router-dom";
+import { Context } from "../store/appContext";
 import { ProfileShelves } from "../component/profileShelves.jsx";
 import { ProfileInfo } from "../component/profileInfo.jsx";
 import { Modal } from "../component/editProfileModal.jsx";
 import "../../styles/profile-view.scss";
 
 export const Profile = () => {
+	const { store, actions } = useContext(Context);
 	const [state, setState] = useState({
 		showModal: false
 	});
+	let idReader = useParams();
+	const button = (
+		<button
+			className="btn btn-primary"
+			onClick={() => {
+				setState({ showModal: true });
+			}}>
+			Editar
+		</button>
+	);
 	return (
 		<div className="profile">
 			<h1>
 				Perfil
-				<span>
-					<button
-						className="btn btn-primary"
-						onClick={() => {
-							setState({ showModal: true });
-						}}>
-						Editar
-					</button>
-				</span>
+				<span>{store.loggedUser == idReader.idUser ? button : ""}</span>
 			</h1>
 			<div>
 				<ProfileInfo />
