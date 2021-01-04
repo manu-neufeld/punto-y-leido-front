b38794062c573de008426ed96cf9c3cbfc73967f
manu-neufeld <manuela.neufeld@gmail.com>
2021-01-04 17:56:48 +0000

    url changed

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,7 +1,7 @@
 import jwt_decode from "jwt-decode";
 
 const getState = ({ getStore, getActions, setStore }) => {
-	let url = "https://3000-a06e473f-9876-434a-94ac-aa7135fbfbc9.ws-eu03.gitpod.io/";
+	let url = "https://3000-f98def42-3e85-4f9c-8105-d366d231e094.ws-eu03.gitpod.io/";
 	return {
 		store: {
 			books: [],
