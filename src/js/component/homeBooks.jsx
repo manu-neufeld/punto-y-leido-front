7a9697213ba3c7ecd1b3401731e42aa0a11e2eb1
manu-neufeld <manuela.neufeld@gmail.com>
2021-01-07 17:52:13 +0000

    card created

--- src/js/component/homeBooks.jsx
+++ src/js/component/homeBooks.jsx
@@ -7,35 +7,27 @@ export const HomeBooks = () => {
 
 	return (
 		<Fragment>
-			<div className="dropdown">
-				<a
-					className="btn btn-secondary dropdown-toggle"
-					href="#"
-					role="button"
-					id="dropdownMenuLink"
-					data-bs-toggle="dropdown"
-					aria-expanded="false">
-					Dropdown link
-				</a>
-
-				<ul className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-					<li>
-						<a className="dropdown-item" href="#">
-							Action
-						</a>
-					</li>
-					<li>
-						<a className="dropdown-item" href="#">
-							Another action
-						</a>
-					</li>
-					<li>
-						<a className="dropdown-item" href="#">
-							Something else here
-						</a>
-					</li>
-				</ul>
+			{/* <div classNameName="books-by-genre-home"> */}
+			<div className="card mb-3">
+				<div className="row g-0">
+					<div className="col-md-4">
+						<img src="..." alt="..." />
+					</div>
+					<div className="col-md-8">
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text">
+								This is a wider card with supporting text below as a natural lead-in to additional
+								content. This content is a little bit longer.
+							</p>
+							<p className="card-text">
+								<small className="text-muted">Last updated 3 mins ago</small>
+							</p>
+						</div>
+					</div>
+				</div>
 			</div>
+			{/* </div> */}
 		</Fragment>
 	);
 };
