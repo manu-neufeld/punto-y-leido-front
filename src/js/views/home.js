60711c3d8ca7ab0ebc6f4f9055e469807c1776bd
manu-neufeld <manuela.neufeld@gmail.com>
2021-01-07 19:05:12 +0000

    home page

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -8,15 +8,19 @@ import { HomeBooks } from "../component/homeBooks.jsx";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
-	return (
-		<Fragment>
-			<div className="home-header">
-				<AboutUsHome />
-				{store.loggedUser !== null ? <WelcomeHome /> : <RegisterLoginWelcome />}
-			</div>
-			<div className="home-body">
-				<HomeBooks />
-			</div>
-		</Fragment>
-	);
+	if (store.books.length == 0 && store.reviews.length == 0) {
+		return <h2>Estamos buscando tu libro...</h2>;
+	} else {
+		return (
+			<Fragment>
+				<div className="home-header">
+					<AboutUsHome />
+					{store.loggedUser !== null ? <WelcomeHome /> : <RegisterLoginWelcome />}
+				</div>
+				<div className="home-body">
+					<HomeBooks />
+				</div>
+			</Fragment>
+		);
+	}
 };
