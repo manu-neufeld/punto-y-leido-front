efed21cdaf4f824d80b41799125392f3c255efd1
manu-neufeld <manuela.neufeld@gmail.com>
2021-01-18 12:10:40 +0000

    random card book in home

--- src/js/component/homeBooks.jsx
+++ src/js/component/homeBooks.jsx
@@ -1,39 +1,49 @@
 import React, { useContext, Fragment } from "react";
-import { useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
-import PropTypes from "prop-types";
 import { ReviewsBookDetails } from "../component/reviewsBookDetails.jsx";
 
-export const HomeBooks = props => {
-	const { store, actions } = useContext(Context);
-
-	return (
-		<Fragment>
-			{/* <div classNameName="books-by-genre-home"> */}
+export const HomeBooks = () => {
+	const { store } = useContext(Context);
+	let bookIndex = [Math.round(Math.random() * (store.books.length - 1))];
+	let reviewIndex = -1;
+	for (let index = 0; index < store.reviews.length; index++) {
+		if (bookIndex == store.reviews[index].id_book) {
+			reviewIndex = index;
+		}
+	}
+	if (store.reviews.lenght == 0) {
+		let bookRandomCard = (
 			<div className="card mb-3">
 				<div className="row g-0">
 					<div className="col-md-4">
-						<img src={props.image} className="card-img" alt="Portada del libro" />
+						<img src={store.books[bookIndex].image} className="card-img" alt="Portada del libro" />
 					</div>
 					<div className="col-md-8">
 						<div className="card-body">
-							<h5 className="card-title">{props.title}</h5>
-							<p className="card-text">
-								<ReviewsBookDetails />
-							</p>
-							{/* <p className="card-text">
-								<small className="text-muted">Last updated 3 mins ago</small>
-							</p> */}
+							<h5 className="card-title">{store.books[bookIndex].title}</h5>
+							<p className="card-text">Todavía no hay reseñas en este libro, ¡Comenta por primera vez!</p>
 						</div>
 					</div>
 				</div>
 			</div>
-			{/* </div> */}
-		</Fragment>
-	);
-};
-
-HomeBooks.propTypes = {
-	title: PropTypes.string,
-	image: PropTypes.string
+		);
+		return bookRandomCard;
+	} else {
+		let bookRandomCard = (
+			<div className="card mb-3">
+				<div className="row g-0">
+					<div className="col-md-4">
+						<img src={store.books[bookIndex].image} className="card-img" alt="Portada del libro" />
+					</div>
+					<div className="col-md-8">
+						<div className="card-body">
+							<h5 className="card-title">{store.books[bookIndex].title}</h5>
+							<p className="card-text">{store.reviews[reviewIndex].review}</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+		return bookRandomCard;
+	}
 };
--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,7 +1,7 @@
 import jwt_decode from "jwt-decode";
 
 const getState = ({ getStore, getActions, setStore }) => {
-	let url = "https://3000-e04bec4a-46a7-4947-acb5-09d976f0597e.ws-eu03.gitpod.io/";
+	let url = "https://3000-b58ecb20-e445-4908-8037-deecde4cbf8b.ws-eu03.gitpod.io/";
 	return {
 		store: {
 			books: [],
